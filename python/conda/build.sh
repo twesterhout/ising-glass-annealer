4bf61b30cd9ade27d7f2f1354d4ded79087f3bbd
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-05-02 18:51:38 +0200

    Fix runtime paths

--- python/conda/build.sh
+++ python/conda/build.sh
@@ -2,23 +2,17 @@
 
 set -ex
 
-project_dir=/home/tom/src/ising-ground-state/
-install -v -m 644 "$project_dir/prefix/include/ising_ground_state.h" "$PREFIX/include"
-install -v -m 644 "$project_dir/prefix/lib/pkgconfig/ising_ground_state.pc" "$PREFIX/lib/pkgconfig"
-install -v -m 644 "$project_dir/prefix/lib/libising_ground_state.so" "$PREFIX/lib/"
+install -v -m 644 "prefix/include/ising_ground_state.h" "$PREFIX/include"
+install -v -m 644 "prefix/lib/pkgconfig/ising_ground_state.pc" "$PREFIX/lib/pkgconfig"
+install -v -m 644 "prefix/lib/libising_ground_state.so" "$PREFIX/lib/"
 
-cd $PREFIX/lib
-ls -l libising*
+find "$PREFIX/lib" -name "*ising_ground_state.so*" -maxdepth 1 -type f | while read -r sofile; do
+  # shellcheck disable=SC2016
+  # echo "Setting rpath of $sofile to" '$ORIGIN'
+  # shellcheck disable=SC2016
+  patchelf --set-rpath "$PREFIX/$HOST/sysroot/lib" --force-rpath "$sofile"
+  # patchelf --replace-needed "/lib64/ld-linux-x86-64.so.2" "$PREFIX/$HOST/sysroot/lib/ld-linux-x86-64.so.2" "$sofile"
+  # ldd "$sofile"
+done
 
-cd $PREFIX/include
-ls -l ising*
-
-# find "prefix/lib" -name "*ising_ground_state.so*" -maxdepth 1 -type f | while read -r sofile; do
-#   # shellcheck disable=SC2016
-#   echo "Setting rpath of $sofile to" '$ORIGIN'
-#   # shellcheck disable=SC2016
-#   patchelf --set-rpath "$PREFIX" --force-rpath "$sofile"
-#   patchelf --print-rpath "$sofile"
-# done
-
-# $PYTHON setup.py install
+$PYTHON setup.py install
