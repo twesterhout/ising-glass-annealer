b73fe1e8ac6dd178169c320817874ccb27a723d9
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-05-02 19:55:28 +0200

    Respect $CC

--- build_shared_library.sh
+++ build_shared_library.sh
@@ -3,6 +3,7 @@
 set -eu
 
 PREFIX="$PWD/prefix"
+LIBRARY_NAME="ising_ground_state"
 declare -a HS_LD_FLAGS
 declare -a C_LD_FLAGS
 
@@ -29,23 +30,39 @@ init_flags() {
 create_shared_library() {
   set -x
   # -optl -Wl,--retain-symbols-file=$PWD/api.txt
-  ghc --make -v -no-hs-main -shared "$(cabal_build_dir)/cbits/init.o" \
-    -o "$PREFIX/lib/libising_ground_state.so" "${HS_LD_FLAGS[@]}" "${C_LD_FLAGS[@]}"
+  ghc --make -v -no-hs-main -pgmc "$CC" -pgml "$CC" -shared "$(cabal_build_dir)/cbits/init.o" \
+    -o "$PREFIX/lib/lib${LIBRARY_NAME}.so" "${HS_LD_FLAGS[@]}" "${C_LD_FLAGS[@]}"
   set +x
 }
 
 create_static_library() {
   # --retain-symbols=$PWD/api.txt
-  ld -o "$PREFIX/lib/libbar.a" --relocatable --whole-archive "${HS_LD_FLAGS[@]}"
+  ld -o "$PREFIX/lib/lib${LIBRARY_NAME}.a" --relocatable --whole-archive "${HS_LD_FLAGS[@]}"
+}
+
+create_pkgconfig_file() {
+  cat >"${PREFIX}/lib/pkgconfig/${LIBRARY_NAME}.pc" <<EOF
+prefix=${PREFIX}
+libdir=${PREFIX}/lib
+includedir=${PREFIX}/include
+
+Name: Dummy
+Description: Dummy
+Version: 0.1.0.0
+Libs: -L\${libdir} -l${LIBRARY_NAME}
+Libs.private:
+Cflags: -I\${includedir}
+EOF
 }
 
 main() {
   init_flags
   mkdir -p "$PREFIX/include"
-  mkdir -p "$PREFIX/lib"
+  mkdir -p "$PREFIX/lib/pkgconfig"
   create_shared_library
   create_static_library
-  install -m 644 "cbits/ising_ground_state.h" "$PREFIX/include"
+  create_pkgconfig_file
+  install -m 644 "cbits/${LIBRARY_NAME}.h" "$PREFIX/include"
 }
 
 main
